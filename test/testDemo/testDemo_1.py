cde8e6a1c1dcf20b7a16cdead119738db34cb77d
summer <weizw5528@126.com>
2018-02-11 17:08:04 +0800

    test

--- test/testDemo/testDemo_1.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-
-browser = webdriver.Firefox()
-browser.get("http://www.baidu.com")
-browser.find_element_by_id("kw").send_keys("network")
-browser.find_element_by_id("su").click()
-browser.quit()#wewewe
